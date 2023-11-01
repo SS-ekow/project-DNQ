b89e9202bcb2bc34bff345db1886715d01356942
Ekow <ekowsackey22@gmail.com>
2023-11-01 23:33:34 +0000

    added a landing page

--- dnq.py
+++ dnq.py
@@ -14,5 +14,9 @@ def signup():
 def signin():
     return render_template('login.html')
 
+@app.route('/home')
+def home():
+    return render_template('landingpage.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
